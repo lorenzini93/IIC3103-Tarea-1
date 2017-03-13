5bceb6863ef14e6194a3b2f96fc758e030ae59e6
lorenzini93 <lelorenzini@uc.cl>
2017-03-13 13:31:00 -0300

    noticia

---
+++ db/migrate/20170313155128_create_noticia.rb
@@ -0,0 +1,13 @@
+class CreateNoticia < ActiveRecord::Migration[5.0]
+  def change
+    create_table :noticia do |t|
+      t.string :titulo
+      t.string :bajada
+      t.txt :cuerpo
+      t.string :link
+      t.int :nComentarios
+
+      t.timestamps
+    end
+  end
+end
