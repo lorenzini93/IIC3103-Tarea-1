a381128c3ee14b69c94d430615cf20a51efb6371
lorenzini93 <lelorenzini@uc.cl>
2017-03-14 00:15:36 -0300

    noticium2

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,16 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+    def noticium_params
+      params.require(:noticium).permit(:titulo, :bajada, :cuerpo, :link, :nComentarios)
+    end
+
+    # GET /noticia
+  # GET /noticia.json
+  def index
+    @noticia = Noticium.all
+  end
+
 end
